755420dd1a864f7cb3c4082004a713432294807c
Allison Collier <10acollier@gmail.com>
2021-08-05 14:45:42 -0400

    begain front end cart logic

--- client/components/Product.js
+++ client/components/Product.js
@@ -7,6 +7,15 @@ class Product extends React.Component {
   async componentDidMount() {
     await this.props.getProduct(this.props.match.params.id);
   }
+ //adding logic for local storage add to cart
+  addItemToCart(productId) {
+    const currentCart = localStorage.getItem(loggedOutUserCart);
+    if (currentCart == null) {
+      currentCart === []
+    }
+    currentCart.push(productId);
+    localStorage.setItem(loggedOutUserCart, currentCart);
+  }
 
   render() {
     const product = this.props.product ?? {};
@@ -17,6 +26,7 @@ class Product extends React.Component {
         <img src={product.imgUrl} width={200} height={200} />
         <p>price: ${product.price}</p>
         {product.description && <p>description: {product.description}</p>}
+        <button className="add-to-cart-button" onclick={()=> addItemToCart(product.id)}>Add To Cart</button>
       </div>
     );
   }
