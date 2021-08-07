29721970aaaeb275f6f3f7b9cd0905fbdd720f6a
ndmetrick <82462717+ndmetrick@users.noreply.github.com>
2021-08-07 16:22:18 -0500

    added authentication

--- server/api/users.js
+++ server/api/users.js
@@ -24,90 +24,112 @@ router.get('/', requireToken, isLoggedIn, isAdmin, async (req, res, next) => {
 });
 
 // ih: need to add gatekeeping functions before 'async' to check if cart belongs to that cart's user, otherwise this route is working
-router.get('/:userId/cart', async (req, res, next) => {
-  try {
-    const cart = await Cart.findOne({
-      include: {
-        model: Product,
-      },
-      where: {
-        userId: req.params.userId,
-      },
-    });
-    if (!cart) {
-      res.sendStatus(404);
+router.get(
+  '/:userId/cart',
+  requireToken,
+  isLoggedIn,
+  async (req, res, next) => {
+    try {
+      if (req.user.id === req.params.userId) {
+        const cart = await Cart.findOne({
+          include: {
+            model: Product,
+          },
+          where: {
+            userId: req.params.userId,
+          },
+        });
+        if (!cart) {
+          res.sendStatus(404);
+        }
+        res.json(cart);
+      } else {
+        res.status(403).send('You are not authorized to view this cart');
+      }
+    } catch (err) {
+      next(err);
     }
-    res.json(cart);
-  } catch (err) {
-    next(err);
   }
-});
+);
 
 // ih: need to add gatekeeping functions before 'async' to check if cart belongs to that cart's user, otherwise this route is working
-router.put('/:userId/addtocart/:productId', async (req, res, next) => {
-  try {
-    const product = await Product.findByPk(req.params.productId);
+router.put(
+  '/:userId/addtocart/:productId',
+  requireToken,
+  isLoggedIn,
+  async (req, res, next) => {
+    try {
+      if (req.user.id === req.params.userId) {
+        const product = await Product.findByPk(req.params.productId);
 
-    const cart = await Cart.findOne({
-      where: {
-        userId: req.params.userId,
-        fulfilled: false,
-      },
-    });
+        const cart = await Cart.findOne({
+          where: {
+            userId: req.params.userId,
+            fulfilled: false,
+          },
+        });
 
-    await cart.addProduct(product);
+        await cart.addProduct(product);
 
-    const productInCart = await Product_Cart.findOne({
-      include: [Cart],
-      where: {
-        cartId: cart.id,
-        productId: product.id,
-      },
-    });
+        const productInCart = await Product_Cart.findOne({
+          include: [Cart],
+          where: {
+            cartId: cart.id,
+            productId: product.id,
+          },
+        });
 
-    productInCart.quantity++;
-    productInCart.subtotalPrice = product.price * productInCart.quantity;
-    cart.totalPrice += product.price;
+        productInCart.quantity++;
+        productInCart.subtotalPrice = product.price * productInCart.quantity;
+        cart.totalPrice += product.price;
 
-    productInCart.save();
-    cart.save();
+        productInCart.save();
+        cart.save();
 
-    res.json(cart);
-  } catch (err) {
-    next(err);
+        res.json(cart);
+      } else {
+        res.status(403).send('You are not authorized to change this cart');
+      }
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 router.put('/:userId/removefromcart/:productId', async (req, res, next) => {
   try {
-    const product = await Product.findByPk(req.params.productId);
+    if (req.user.id === req.params.userId) {
+      const product = await Product.findByPk(req.params.productId);
 
-    const cart = await Cart.findOne({
-      where: {
-        userId: req.params.userId,
-        fulfilled: false,
-      },
-    });
+      const cart = await Cart.findOne({
+        where: {
+          userId: req.params.userId,
+          fulfilled: false,
+        },
+      });
 
-    const productInCart = await Product_Cart.findOne({
-      include: [Cart],
-      where: {
-        cartId: cart.id,
-        productId: product.id,
-      },
-    });
+      const productInCart = await Product_Cart.findOne({
+        include: [Cart],
+        where: {
+          cartId: cart.id,
+          productId: product.id,
+        },
+      });
 
-    productInCart.quantity--;
-    productInCart.subtotalPrice = product.price * productInCart.quantity;
-    if (!productInCart.quantity) {
-      await cart.removeProduct(product);
+      productInCart.quantity--;
+      productInCart.subtotalPrice = product.price * productInCart.quantity;
+      if (!productInCart.quantity) {
+        await cart.removeProduct(product);
+      }
+      cart.totalPrice -= product.price;
+
+      productInCart.save();
+      cart.save();
+
+      res.json(cart);
+    } else {
+      res.status(403).send('You are not authorized to change this cart');
     }
-    cart.totalPrice -= product.price;
-
-    productInCart.save();
-    cart.save();
-
-    res.json(cart);
   } catch (err) {
     next(err);
   }
